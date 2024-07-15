2cfed540e735ead463252c5f83a9a286b0202f08
ELIJAH Kinyamal <94557540+elijahkinyamal@users.noreply.github.com>
2024-07-15 20:00:14 +0300

    Update features.js

--- src/components/features.js
+++ src/components/features.js
@@ -7,11 +7,11 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 export default function Features() {
   return (
     <ScrollView style={{height: hp(60)}} bounces={false} showsVerticalScrollIndicator={false} className="space-y-4">
-        <Text style={{fontSize: wp(6.5)}} className="font-semibold text-gray-700">Features</Text>
+        <Text style={{fontSize: wp(6.6)}} className="font-semibold text-gray-700">Features</Text>
         <View className="bg-emerald-200 p-4 rounded-xl space-y-2">
             <View className="flex-row items-center space-x-1">
                 <Image className="rounded-ful" source={require('../../assets/images/chatgptIcon.png')} style={{height: hp(4), width: hp(4)}} />
-                <Text style={{fontSize: wp(4.8)}} className="font-semibold text-gray-700">ChatGPT</Text>
+                <Text style={{fontSize: wp(4.9)}} className="font-semibold text-gray-700">ChatGPT</Text>
             </View>
             
             <Text style={{fontSize: wp(3.8)}} className="text-gray-700 font-medium">
